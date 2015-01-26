aaeac6948e196f8966b0618c605cd5edb00c65f1
Ryan Roseman <ryanroseman@yahoo.com>
2015-01-26 12:19:49 -0600

    Goliath Online

--- cs4743a1/assignment1/src/assignment1/Tester.java
+++ cs4743a1/assignment1/src/assignment1/Tester.java
@@ -3,7 +3,7 @@ package assignment1;
 import java.io.IOException;
 
 public class Tester {
-	
+	// Test
 	public static void main(String args[]) {
 		PartsInventoryModel pim = new PartsInventoryModel();
 	//	PartsInventoryView pview = new PartsInventoryView(pim);
